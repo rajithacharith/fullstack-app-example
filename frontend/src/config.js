09c389c88bf476afd23d9a7952f32195eb80be27
rajithacharith <rajithacharith@gmail.com>
2025-05-23 13:03:32 +0530

    Template config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,9 @@
+// config.js
+// This file will be mounted as a config map in deployment environments.
+// Update the API_BASE_URL as needed for each environment.
+
+const config = {
+  API_BASE_URL: 'http://localhost:5000',
+};
+
+export default config;
